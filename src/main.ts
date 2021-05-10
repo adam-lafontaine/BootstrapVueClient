712643b1ef28ba7e1263d621b1d71df5d9b44597
adam-lafontaine <adam.lafontaine@gmail.com>
2021-05-10 18:42:07 -0400

    add test component

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import 'mutationobserver-shim'
 import Vue from 'vue'
 import './plugins/bootstrap-vue'
 import App from './App.vue'
-import router from './router'
+import router from './router/router'
 import store from './store/store'
 
 Vue.config.productionTip = false
---
+++ src/views/Test.vue
@@ -0,0 +1,26 @@
+<style lang="scss"></style>
+
+<template>
+    <div class="text-center my-3">
+        <b-button
+            @click="load_test_data"
+            >
+            Load
+        </b-button>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import { component } from 'vue/types/umd';
+
+@Component({components: {}})
+export default class Test extends Vue
+{
+
+    private load_test_data(): void
+    {
+        alert("load");
+    }
+}
+</script>
